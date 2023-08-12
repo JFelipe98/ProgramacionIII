03536dfebe74e47e1338f889e4ef01bb5e154e80
JFelipe98 <jfciros@uqvirtual.edu.co>
2023-08-12 17:11:34 -0500

    Se cambia el contenido del main

--- src/ejemplo.java
+++ src/ejemplo.java
@@ -2,16 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class ejemplo {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 9; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.print("Este mensaje es nuevo");
         }
     }
-}
